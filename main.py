7a3f9f1518f6c3b42056c67170dad5a063f457ae
mileRANT <liem.htran@outlook.com>
2023-04-20 15:47:55 -0400

    Adjusted api call to new standards as line-items pricing have changed

--- main.py
+++ main.py
@@ -24,5 +24,58 @@ def get_publishable_key():
     stripe_config = {"publicKey": stripe_keys["publishable_key"]}
     return jsonify(stripe_config)
 
+@app.route("/success")
+def success():
+    return render_template("success.html")
+
+
+@app.route("/cancelled")
+def cancelled():
+    return render_template("cancelled.html")
+
+@app.route("/create-checkout-session")
+def create_checkout_session():
+    domain_url = "http://127.0.0.1:5000/"
+    stripe.api_key = stripe_keys["secret_key"]
+
+    try:
+        # Create new Checkout Session for the order
+        # Other optional params include:
+        # [billing_address_collection] - to display billing address details on the page
+        # [customer] - if you have an existing Stripe Customer ID
+        # [payment_intent_data] - capture the payment later
+        # [customer_email] - prefill the email input in the form
+        # For full details see https://stripe.com/docs/api/checkout/sessions/create
+
+        # ?session_id={CHECKOUT_SESSION_ID} means the redirect will have the session ID set as a query param
+        checkout_session = stripe.checkout.Session.create(
+            success_url=domain_url + "success?session_id={CHECKOUT_SESSION_ID}",
+            cancel_url=domain_url + "cancelled",
+            payment_method_types=["card"],
+            mode="payment",
+            # line_items=[
+            #     {
+            #         "name": "Test item",
+            #         "quantity": 1,
+            #         "currency": "usd",
+            #         "amount": "2000",
+            #     }
+            # ],        #stripe has updated to put everything in price_data which is in line_items
+            line_items=[{
+                  "price_data": {
+                    "currency": "usd",
+                    "unit_amount": 500,
+                    "product_data": {
+                      "name": "name of the product",
+                    },
+                  },
+                  "quantity": 1,
+                }
+              ]
+        )
+        return jsonify({"sessionId": checkout_session["id"]})
+    except Exception as e:
+        return jsonify(error=str(e)), 403
+
 if __name__ == "__main__":
     app.run()
